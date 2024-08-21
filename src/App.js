f292cd354cb6190e1674cbca5f075a63b49192e7
Denis <dalekseenko8@gmail.com>
2024-08-21 18:15:29 +0300

    react_form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import { useTelegram } from './hooks/useTelegram';
+import {Route, Routes} from 'react-router-dom'
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 const tg = window.Telegram.WebApp
 function App() {
   const {onToggleButton, tg} = useTelegram()
@@ -12,7 +15,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <button onClick={onToggleButton}>toggle</button>
+      <Routes>
+        <Route index element={<ProductList/>}/>
+        <Route path={'form'} element={<Form/>}/>
+      </Routes>
     </div>
   );
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Button.css'
 const button = (props) => {
     return (
-        <button {...props} className={'button' + props.className}>
+        <button {...props} className={'button'}>
         </button>
     )
 }
